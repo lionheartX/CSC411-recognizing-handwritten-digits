c41990ddef3cfc6baf956bb2cd794f553f8f635b
lionheartX <lionheart.xiong@gmail.com>
2016-02-11 09:42:23 -0500

    kNN classfication rate with different values of k
    kNN classfication rate with different values of k

--- run_knn.py
+++ run_knn.py
@@ -1,5 +1,7 @@
 import numpy as np
 from l2_distance import l2_distance
+import matplotlib.pyplot as pyplot
+from utils import *
 
 def run_knn(k, train_data, train_labels, valid_data):
     """Uses the supplied training inputs and labels to make
@@ -37,3 +39,25 @@ def run_knn(k, train_data, train_labels, valid_data):
     valid_labels = valid_labels.reshape(-1,1)
 
     return valid_labels
+
+if __name__ == '__main__':
+    train_inputs, train_targets = load_train()
+    valid_inputs, valid_targets = load_valid()
+
+    num_valid = valid_targets.size
+    classification_rates_list = []
+    
+    ks = [1, 3, 5, 7, 9]
+    for k in ks:
+        valid_labels = run_knn(k, train_inputs, train_targets, valid_inputs)
+        num_mismatches = np.sum(np.logical_xor(valid_labels, valid_targets))
+        # since the output is either 1 or 0, we can simply xor
+        # print num_mismatches
+        classification_rate = 1.0 - num_mismatches / (num_valid * 1.0)
+        classification_rates_list.append(classification_rate)
+        
+    pyplot.title("kNN classification rate") 
+    pyplot.xlabel("k")
+    pyplot.ylabel("classification rate")
+    pyplot.plot(ks, classification_rates_list)
+    pyplot.show()
